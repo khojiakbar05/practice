8813b40d6fc790c6c17cf38960a9543c09074342
khojiakbar05 <karimjonovkhojiakbar1@gmail.com>
2026-04-15 10:49:37 +0900

    feat: python system variables & special methods via dunderss

--- run.py
+++ run.py
@@ -1,1 +1,24 @@
-print("Hello, World!")
+# Primitive variables
+
+# print("PYTHON: Everything is Object")
+
+# DUNDERS(Double Underscore)   &   __BUILTINS__ ,  &  __init__
+
+# Dunder -> Pythonni ichki  quriliah mexanizmi
+
+
+message = "PYTHON: Everything is Object"
+print(message)
+
+result = type(message)
+print("result: ", result)
+
+'''
+In Python, there are builtin tools:
+
+(1) TYPES => int float str list dict
+(2) FUNCTIONS => print() type() len() range() input() etc.  
+(3) CONATANTS => None True False   
+'''
+
+print(dir(__builtins__))
