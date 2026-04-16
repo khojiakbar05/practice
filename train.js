b99364375acffd50f1dcf5ca0a918c92abca32b2
khojiakbar05 <karimjonovkhojiakbar1@gmail.com>
2026-04-16 11:17:21 +0900

    fix: A-TASK (Node.js)

--- train.js
+++ train.js
@@ -1,3 +1,9 @@
+// A-TASK (NodeJS)
+/*
+Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
+MASALAN countLetter("e", "engineer") 3ni return qiladi.
+*/
+// Masalani yechimi
 // Define (parametr)
 function countLetter(letter, word) {
  let count = 0;
