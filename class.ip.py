b87340fc10d1035eb87131e576f9cbc6f0c6e940
khojiakbar05 <karimjonovkhojiakbar1@gmail.com>
2026-04-19 05:38:50 +0900

    feat: Polymorphism concept

--- class.ip.py
+++ class.ip.py
@@ -6,73 +6,75 @@
 
 print("========== Inheritance =============")
 # Parent > child
-# Parent only provides Public & protected properties(state + method) to cild! 
+# Parent only provides Public & protected properties(state + method) to cild!
+
 
 class Animal:     # Parent class
-       # State
-       description = "The class creates animals"
+    # State
+    description = "The class creates animals"
 
-       # Constructure
-       def __init__(self, voice):
-              self._status = "Animal is Alive"
-              self.voice = voice
+    # Constructure
+    def __init__(self, voice):
+        self._status = "Animal is Alive"
+        self.voice = voice
 
-       # method
-       def make_voice(self):
-              print(f"The animal make a voice {self.voice}")
-
-
+    # method
+    def make_voice(self):
+        print(f"The animal make a voice {self.voice}")
 
 
 class Dog(Animal):     # Child class
-       # state
+    # state
 
-       # constructure
-       def __init__(self, name, sound, voice):
-              self.name = name
-              self.sound = sound
-              super().__init__(voice)
+    # constructure
+    def __init__(self, name, sound, voice):
+        self.name = name
+        self.sound = sound
+        super().__init__(voice)
 
-       # method
-       def introduce(self):
-           print(f"{self.name} says: {self.sound}-{self.sound}")
+    # method
+    def introduce(self):
+        print(f"{self.name} says: {self.sound}-{self.sound}")
 
-       def protect(self):
-              print("Yes, I can protect you")
+    def protect(self):
+        print("Yes, I can protect you")
+
+    def make_voice(self):
+        print(f"The {self.name} says:  {self.sound}")
 
 
 class Cat(Animal):    # Child class
-      # state
+    # state
 
-      # constructure
-      def __init__(self, name, sound, voice):
-             self.name = name
-             self.sound = sound
-             super().__init__(voice)
+    # constructure
+    def __init__(self, name, sound, voice):
+        self.name = name
+        self.sound = sound
+        super().__init__(voice)
 
-       # method
-      def introduce(self):
-           print(f"{self.name} says: {self.sound}-{self.sound}")
+    # method
+    def introduce(self):
+        print(f"{self.name} says: {self.sound}-{self.sound}")
 
-      def play(self):
-              pass
+    def play(self):
+        pass
 
 
 class Fish(Animal):     # Child class
-      # state
+    # state
 
-      # constructure
-      def __init__(self, name, sound, voice):
-             self.name = name
-             self.sound = sound
-             super().__init__(voice)
+    # constructure
+    def __init__(self, name, sound, voice):
+        self.name = name
+        self.sound = sound
+        super().__init__(voice)
 
-       # method
-      def introduce(self):
-           print(f"{self.name} says: {self.sound}-{self.sound}")
+    # method
+    def introduce(self):
+        print(f"{self.name} says: {self.sound}-{self.sound}")
 
-      def swim(self):
-              print("Yes, I can protect you")
+    def swim(self):
+        print("Yes, I can protect you")
 
 
 dog = Dog("Rex", "wow", True)
@@ -92,8 +94,30 @@ fish.make_voice()
 
 print("--------")
 
-print("Parent class: ",Animal.description)
-
+print("Parent class: ", Animal.description)
 print("Child class: ", Dog.description)
-
 print("dog.status: ", dog._status)
+
+
+print("========== Polymorphism =============")
+
+
+dog.make_voice()
+fish.make_voice()
+
+
+print("--------")
+
+# fish > fish > Animal > object
+a = isinstance(fish, Fish)
+b = isinstance(fish, Animal)
+c = isinstance(fish, object)
+d = isinstance("MIT", object)
+result = a and b and c and d
+print("The result: ", {result})
+
+# Fish > Animal > object
+data1 = issubclass(Fish, Animal)
+data2 = issubclass(Animal, object)
+print("data: ", data1, data2)
+
