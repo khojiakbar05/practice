239a2bfbe5f7ccaed1312e59ac8e1dce7840733f
khojiakbar05 <karimjonovkhojiakbar1@gmail.com>
2026-04-19 00:11:13 +0900

    feat: B-TASK (NodeJs) completed

--- ex.py
+++ ex.py
@@ -6,10 +6,6 @@
 #        print("The email is not valid")
 
 
-
-
-
-
 # age = input("Enter your age: ")
 # if age.isdigit():
 #        print("The age is valid")
@@ -21,9 +17,34 @@
 # print("list: ", list1[len(list1)-1])
 
 
+i = 100
+while (i <= 200):
+    print(i)
+    i += 20
+
+a = "my name is khoji"
+result = a.capitalize()
+print(result)
+
+s1 = {1, 2, 3, 4, 5, 6}
+s2 = {6, 7, 8, 9}
+
+print(s1 & s2)
+
+
+income = 500
+if income > 2500:
+    tax = 500
+else:
+    tax = 200
+    print("income ", tax)
 
 
 
 
 
+name = input("What is your name: ")
+age = int(input("How old are you: "))
+year = 2005 - age + 100
 
+print(name + ", you will be 100 years old in the year " + str(year))
--- train.js
+++ train.js
@@ -5,15 +5,35 @@ MASALAN countLetter("e", "engineer") 3ni return qiladi.
 */
 // Masalani yechimi
 // Define (parametr)
-function countLetter(letter, word) {
- let count = 0;
-  for (let i = 0; i < word.length; i++) {
-    if (word[i] === letter) {
-      count ++;
+// function countLetter(letter, word) {
+//  let count = 0;
+//   for (let i = 0; i < word.length; i++) {
+//     if (word[i] === letter) {
+//       count ++;
+//     }
+//   }
+//   return count;
+// }
+
+// // Call (argument)
+// console.log(countLetter("e", "engineer"));
+
+//. B-TASK (NodeJS)
+/*
+Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
+MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
+ */
+
+// masalani yechimi
+// Define (parametr)
+function countDigits(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] >= "0" && str[i] <= "9") {
+      count++;
     }
   }
   return count;
 }
 
-// Call (argument)
-console.log(countLetter("e", "engineer")); 
+console.log(countDigits("ad2a54y79wet3sfgb9"));
