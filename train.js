8bdfd6a96b6f3d1c5c74d20c4f56315fb05e341f
khojiakbar05 <karimjonovkhojiakbar1@gmail.com>
2026-04-23 08:49:50 +0900

    feat: D - TASK (NodeJs) completed

--- train.js
+++ train.js
@@ -1,21 +1,45 @@
+// D-TASK (NodeJS)
+
+// Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
+// MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
+
+// masalani yechimi
+function getHighestIndex(arr) {
+  let max = 0;
+  let maxIndex = [0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i]
+      maxIndex = i
+    }
+  }
+  return maxIndex
+}
+
+console.log(getHighestIndex([5, 21, 12, 21, 8]));
+
+
+
+
 //.  C-TASK (NodeJS)
 /*
 Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin
 MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;
  */
 // masalani yechimi
-function checkContent(str1, str2) {
-  if (str1.length !== str2.length) {
-    return False;
-  }
+// function checkContent(str1, str2) {
+//   if (str1.length !== str2.length) {
+//     return False;
+//   }
 
-  const sortedStr1 = str1.toLowerCase().split(``).sort().join(``);
-  const sortedStr2 = str2.toLowerCase().split(``).sort().join(``);
+//   const sortedStr1 = str1.toLowerCase().split(``).sort().join(``);
+//   const sortedStr2 = str2.toLowerCase().split(``).sort().join(``);
 
-  return sortedStr1 === sortedStr2;
-}
+//   return sortedStr1 === sortedStr2;
+// }
 
-console.log(checkContent("mitgroup", "Ourgpimt"));
+// console.log(checkContent("mitgroup", "Ourgpimt"));
 
 
 
